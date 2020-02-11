6615562c27d2ff38cbd1f604820213c9e806ad5b
Kenni <kens123@hotmail.dk>
2020-02-11 12:23:30 +0100

    Fixed some shit

--- app/src/main/java/com/example/trendlog/Main2Activity.kt
+++ app/src/main/java/com/example/trendlog/Main2Activity.kt
@@ -4,25 +4,28 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.content.Intent
 import android.view.View
+import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_main2.*
 
 class Main2Activity : AppCompatActivity() {
 
     lateinit var textView: TextView
+    lateinit var button: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
 
-        val testString = intent.getStringExtra("FullName")
         textView = findViewById(R.id.textView2)
+        button = findViewById(R.id.button)
+
+        val testString = intent.getStringExtra("FullName")
         textView.text = testString
-
-
+        button.setOnClickListener{
+            Toast.makeText(this,"$testString er sej", Toast.LENGTH_LONG).show()
+        }
     }
-
-
-
 }
